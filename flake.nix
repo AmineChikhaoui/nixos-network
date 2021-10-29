3cc478ec433e1114eab2ff4cae06b2e557218e82
Amine Chikhaoui <amine@chikhaoui.org>
2021-10-29 00:30:28 -0400

    minor formatting fix

--- flake.nix
+++ flake.nix
@@ -7,13 +7,11 @@
     home-manager.url = "github:nix-community/home-manager/release-21.05";
 
     # Secrets management
-    sops-nix.url = github:Mic92/sops-nix;
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
   outputs =
-    flakes @ {
-      self, nixpkgs, unstablepkgs, hydra, home-manager, sops-nix
-    }:
+    flakes@{ self, nixpkgs, unstablepkgs, hydra, home-manager, sops-nix }:
 
     let
       unstablePkgs = system:
@@ -24,63 +22,55 @@
 
       unstablex86Pkgs = unstablePkgs "x86_64-linux";
       unstableAarch64Pkgs = unstablePkgs "aarch64-linux";
-    in
-    {
-      nixosConfigurations =
-        {
-          nixos =
-            nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                (import ./laptop/configuration.nix unstablex86Pkgs)
-                (
-                  { environment.systemPackages = [
-                      home-manager.defaultPackage.x86_64-linux
-                    ];
-                  }
-                )
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.amine = import ./laptop/home.nix;
-                }
-                sops-nix.nixosModules.sops {
-                  sops.defaultSopsFile = ./secrets.yaml;
-                  sops.age.keyFile = "/home/amine/.config/sops/age/keys.txt";
-                }
-            ];
-          };
-
-          linode =
-            nixpkgs.lib.nixosSystem
+    in {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (import ./laptop/configuration.nix unstablex86Pkgs)
+            ({
+              environment.systemPackages =
+                [ home-manager.defaultPackage.x86_64-linux ];
+            })
+            home-manager.nixosModules.home-manager
             {
-              system = "x86_64-linux";
-              modules = [
-                (import ./linode/configuration.nix unstablex86Pkgs)
-                # sops-nix.nixosModule {
-                #   sops.defaultSopsFile = ./secrets.yaml;
-                #   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-                #   sops.age.generateKey = true;
-                # }
-              ];
-            };
-
-          # Oracle Cloud free tier aarch64 box
-          monitoring =
-            nixpkgs.lib.nixosSystem
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.amine = import ./laptop/home.nix;
+            }
+            sops-nix.nixosModules.sops
             {
-              system = "aarch64-linux";
-              modules = [
-                (import ./monitoring/configuration.nix unstableAarch64Pkgs)
-                sops-nix.nixosModules.sops {
-                  sops.defaultSopsFile = ./secrets.yaml;
-                  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-                  sops.age.generateKey = true;
-                }
-              ];
-            };
-
+              sops.defaultSopsFile = ./secrets.yaml;
+              sops.age.keyFile = "/home/amine/.config/sops/age/keys.txt";
+            }
+          ];
         };
+
+        linode = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (import ./linode/configuration.nix unstablex86Pkgs)
+            # sops-nix.nixosModule {
+            #   sops.defaultSopsFile = ./secrets.yaml;
+            #   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+            #   sops.age.generateKey = true;
+            # }
+          ];
+        };
+
+        # Oracle Cloud free tier aarch64 box
+        monitoring = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          modules = [
+            (import ./monitoring/configuration.nix unstableAarch64Pkgs)
+            sops-nix.nixosModules.sops
+            {
+              sops.defaultSopsFile = ./secrets.yaml;
+              sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+              sops.age.generateKey = true;
+            }
+          ];
+        };
+      };
     };
 }
