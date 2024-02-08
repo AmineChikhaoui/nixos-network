03004ee5791268642f3f120d930087d215ffa6bb
Amine Chikhaoui <amine@chikhaoui.org>
2024-02-08 09:29:13 -0500

    remove work references

--- laptop/networking.nix
+++ laptop/networking.nix
@@ -10,12 +10,6 @@
 
     #resolvconf.extraOptions = [ "rotate" ];
 
-    search = [
-      "logicblox.com"
-      "predictix.com"
-      "dev.retail.infor.com"
-    ];
-
     nameservers = lib.mkOverride 0 [
       "8.8.8.8" # Google
       "100.100.100.100" # Tailscale
